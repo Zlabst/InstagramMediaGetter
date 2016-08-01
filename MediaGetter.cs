29c1109f5c6eff1350534f387774f5f38d7969d3
matkovd <daniil.matkov@gmail.com>
2016-08-01 22:44:32 +0300

    fix codestyle

--- MediaGetter.cs
+++ MediaGetter.cs
@@ -3,6 +3,7 @@ using System.Net;
 using System.Net.Http;
 using System.IO;
 using Newtonsoft.Json;
+
 namespace instagramMediaGetter
 {
 	public class MediaGetter
@@ -10,28 +11,32 @@ namespace instagramMediaGetter
 		private string id;
 		public string username;
 
-		public MediaGetter (string user)
+		public MediaGetter(string user)
 		{
 			username = user;
 			getBasicInfo();
-			
+
 		}
-		private void getBasicInfo(){
+
+		private void getBasicInfo()
+		{
 			string html = string.Empty;
 			string url = @"https://instagram.com/" + username + "/?__a=1";
 
-			HttpWebRequest request = (HttpWebRequest)WebRequest.Create (url);
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
 			request.AutomaticDecompression = DecompressionMethods.GZip;
 
-			using (HttpWebResponse response = (HttpWebResponse)request.GetResponse ())
-			using (Stream stream = response.GetResponseStream ())
-			using (StreamReader reader = new StreamReader (stream)) {
-				html = reader.ReadToEnd ();
+			using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+			using (Stream stream = response.GetResponseStream())
+			using (StreamReader reader = new StreamReader(stream))
+			{
+				html = reader.ReadToEnd();
 
-				dynamic data = JsonConvert.DeserializeObject (html);
+				dynamic data = JsonConvert.DeserializeObject(html);
 				id = data.user.id;
 			}
 		}
+
 		public string getUserId()
 		{
 			return id;
